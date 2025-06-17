d37fe232e64cc6af41d4041639b030407c90da25
Krosenvag <herve.leguennec13@gmail.com>
2025-06-17 22:26:57 +0200

    Remove user-related methods from HBnBFacade class to streamline functionality

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -13,45 +13,6 @@ class HBnBFacade:
         self.amenity_repo = SQLAlchemyRepository(Amenity)
 
 
-    # USER METHODS
-
-    def create_user(self, user_data):
-        """
-        Create a new user after checking for duplicate email
-        Raises ValueError if email already exists
-        """
-        # Check for duplicate email before creating user
-        existing_user = self.get_user_by_email(user_data['email'])
-        if existing_user:
-            raise ValueError("Email already registered")
-
-        # Create and validate new user
-        user = User(**user_data)
-        user.checking()
-        self.user_repo.add(user)
-        return user
-
-    def get_user(self, user_id):
-        return self.user_repo.get(user_id)
-    
-    def get_user_by_id(self, user_id):
-        return self.user_repository.get(user_id)
-
-    def get_user_by_email(self, email):
-        """Get user by email"""
-        return next(
-            (user for user in self.user_repo.get_all() if user.email == email),
-            None
-        )
-
-    def get_all_users(self):
-        return self.user_repo.get_all()
-
-    def update_user(self, user_id, user_data):
-        user = User(**user_data)
-        user.checking()
-        self.user_repo.update(user_id, user_data)
-        return user
 
     # AMENITY METHODS
 
