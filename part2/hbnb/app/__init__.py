aeb7e528b8e5ec5801d3c4b60e772b52059f2e2d
Krosenvag <herve.leguennec13@gmail.com>
2025-06-18 13:39:27 +0200

    Implement user and place namespaces: add user and place API namespaces to the application

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -5,7 +5,12 @@ def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-    # Placeholder for API namespaces (endpoints will be added later)
-    # Additional namespaces for places, reviews, and amenities will be added later
+    # Import and add namespaces
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.places import api as places_ns
+    # Add more namespaces as you implement them
+
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(places_ns, path='/api/v1/places')
 
     return app
