ca7c29cdb9bd31f36d5e3a24db899f672715e9b1
Krosenvag <herve.leguennec13@gmail.com>
2025-06-20 09:23:21 +0200

    Add amenities and reviews namespaces to the API

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -8,9 +8,12 @@ def create_app():
     # Import and add namespaces
     from app.api.v1.users import api as users_ns
     from app.api.v1.places import api as places_ns
-    # Add more namespaces as you implement them
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.reviews import api as reviews_ns
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
     return app
