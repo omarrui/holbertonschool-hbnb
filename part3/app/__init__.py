69e23115a3c4dc38f709272ba0fedf01de5a70f2
Krosenvag <herve.leguennec13@gmail.com>
2025-06-30 12:19:29 +0200

    feat: add Flask app structure with API and database configuration

---
+++ part3/app/__init__.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask_restx import Api
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy 
+from app.extensions import db, bcrypt, jwt
+
+jwt = JWTManager()
+bcrypt = Bcrypt()
+from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as place_ns
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_ns
+from app.api.v1.protected import api as protected_ns
+     
+def create_app(config_class="config.DevelopmentConfig"):
+    # Créer l'objet Flask d'abord
+    app = Flask(__name__)
+    
+    # Puis configurer l'application
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hbnb.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object(config_class)
+    
+    # Créer l'API
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API',
+              security='Bearer Auth', authorizations={
+                  'Bearer Auth': {
+                      'type': 'apiKey',
+                      'in': 'header',
+                      'name': 'Authorization',
+                      'description': "Jwt authorization header"
+                  }
+              })
+              
+    # Initialiser les extensions
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    
+    # Register the namespaces
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(place_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
+    api.add_namespace(protected_ns, path='/api/v1/protected')
+
+    # Créer les tables
+    with app.app_context():
+        db.create_all()
+        
+    return app
--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -1,11 +1,16 @@
 import os
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
+    JWT_TIME_DELTA = timedelta(seconds=-10)
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
     'development': DevelopmentConfig,
