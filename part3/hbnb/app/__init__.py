2aac8b4799af1ae98af6763796aa44a534726b24
omarrui <rouiguio919@gmail.com>
2025-07-01 23:58:25 +0200

    feat: Protect GET /api/v1/users/<user_id> endpoint with JWT authentication
    - Added @jwt_required() decorator to enforce authentication.
    - Used get_jwt_identity() to retrieve the user's identity from the JWT token.
    - Ensured only authenticated users can access user details.

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_restx import Api
 from app.extensions import db, bcrypt, jwt
+from flask_jwt_extended import JWTManager
+
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -21,11 +24,13 @@ def create_app(config_class="config.DevelopmentConfig"):
     from app.api.v1.places import api as places_ns
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.protected import api as protected_ns
     
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(protected_ns, path='/api/v1/protected')
     
     # Create database tables
     with app.app_context():
